0d922faa0b1655a7205503410cd3f24316fe67c8
jeein <jeein@DESKTOP-Q5LCBGO>
2024-02-24 21:48:46 +0530

    Count number of odd and even numbers in given number.

--- src/javaprograms/CountNumberOfEvenAndOdd.java
+++ src/javaprograms/CountNumberOfEvenAndOdd.java
@@ -3,14 +3,14 @@ package javaprograms;
 public class CountNumberOfEvenAndOdd {
 
 	public static void main(String[] args) {
-// checking each digit in number is even or add
-int a= 123456;
+// checking each digit in number is even or odd
+int originalnumb= 123456;
 int even =0;
 int odd =0;
 
-while(a>0)
+while(originalnumb>0)
 {
-	int b =a%10;
+	int b =originalnumb%10;
 	if(b%2==0)
 		{
 			even++;
@@ -19,7 +19,7 @@ while(a>0)
 		{
 			odd++;
 		}
-	a=a/10;
+	originalnumb=originalnumb/10;
 	
 	
 }
